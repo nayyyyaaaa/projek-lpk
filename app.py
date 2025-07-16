957c60d8acf16b9b62fc393a9df4814424a00c0b
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-16 16:58:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ bobot_molekul_titrat = s.number_input("BE")
 hasil = N(bobot_titrat_mg, faktor_pengali, volume_titran, bobot_molekul_titrat)
 hasil = round(hasil, 4)
 
-s.write(hasil)
+# s.write(hasil)
 
 def kadar(volume_titran_kadar, konsentrasi_titran_kadar, BE_kadar, faktor_pengali_kadar, volume_titrat):
 
