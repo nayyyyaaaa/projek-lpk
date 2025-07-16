ed4f7c8a9510ab9086eee0a591bb623714311215
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-16 15:58:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as s
 
 bobot_titrat_mg = s.number_input("Bobot Titrat (mg)")
+faktor_pengali = s.number_input("faktor pengali")
+volume_titran = s.number_input("volume titran")
+bobot_molekul_titrat = s.number_input("BE")
