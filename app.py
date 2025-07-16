3cf6e36fab788c754e1f7ab77a9817c90961d2ca
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-16 16:06:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as s
 
+def N(bobot_titrat, faktor_pengali, volume_titran, BE):
+
+  hasil = bobot_titrat/(faktor_pengali*volume_titran*BE)
+  
+  return hasil
+
 bobot_titrat_mg = s.number_input("Bobot Titrat (mg)")
 faktor_pengali = s.number_input("faktor pengali")
 volume_titran = s.number_input("volume titran")
 bobot_molekul_titrat = s.number_input("BE")
+
+hasil = N(bobot_titrat_mg, faktor_pengali, volume_titran, bobot_molekul_titrat)
+
+s.write(hasil)
