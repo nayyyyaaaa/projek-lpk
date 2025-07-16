44b6aba426017371cc1ea262a5af437b3b53a10e
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-16 18:00:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ s.title("Aplikasi Perhitungan Titrasi")
 # Fungsi Perhitungan
 # ----------------------------
 
-def N(bobot_titrat, faktor_pengali, volume_titran, BE):
-    return bobot_titrat / (faktor_pengali * volume_titran * BE)
+def N(bobot_titrat_gram, faktor_pengali, volume_titran, BE):
+    return bobot_titrat_gram / (faktor_pengali * volume_titran * BE)
 
 def kadar(volume_titran_kadar, konsentrasi_titran_kadar, BE_kadar, faktor_pengali_kadar, volume_titrat):
     return (volume_titran_kadar * konsentrasi_titran_kadar * BE_kadar * 0.1 * faktor_pengali_kadar) / volume_titrat
@@ -22,14 +22,14 @@ def hitung_rpd(c1, c2):
 # ----------------------------
 s.header("Perhitungan Konsentrasi")
 
-bobot_titrat_mg = s.number_input("Bobot Titrat (mg)", format="%.4f", key="bobot_titrat")
-faktor_pengali = s.number_input("Faktor pengali", format="%.4f", key="faktor_pengali")
+bobot_titrat_gram = s.number_input("Bobot Titrat (gram)", format="%.4f", key="bobot_titrat_gram")
+faktor_pengali = s.number_input("Faktor pengali", step=1, format="%d", key="faktor_pengali")  # hanya bilangan bulat
 volume_titran = s.number_input("Volume titran", format="%.4f", key="volume_titran")
 bobot_molekul_titrat = s.number_input("BE", format="%.4f", key="BE")
 
 hasil = 0.0
 if faktor_pengali != 0 and volume_titran != 0 and bobot_molekul_titrat != 0:
-    hasil = N(bobot_titrat_mg, faktor_pengali, volume_titran, bobot_molekul_titrat)
+    hasil = N(bobot_titrat_gram, faktor_pengali, volume_titran, bobot_molekul_titrat)
     hasil = round(hasil, 4)
     s.success(f"Hasil Konsentrasi: {hasil} N")
 
@@ -40,7 +40,7 @@ s.header("Perhitungan Kadar")
 
 volume_titran_kadar = s.number_input("Volume titran (kadar)", format="%.4f", key="vol_titran_kadar")
 bobot_molekul_titrat_kadar = s.number_input("BE (kadar)", format="%.4f", key="BE_kadar")
-faktor_pengali_kadar = s.number_input("Faktor pengali (kadar)", format="%.4f", key="faktor_pengali_kadar")
+faktor_pengali_kadar = s.number_input("Faktor pengali (kadar)", step=1, format="%d", key="faktor_pengali_kadar")
 volume_titrat_kadar = s.number_input("Volume titrat (kadar)", format="%.4f", key="vol_titrat_kadar")
 
 hasil2 = 0.0
@@ -53,7 +53,7 @@ if (
 ):
     hasil2 = kadar(
         volume_titran_kadar,
-        hasil,  # pakai hasil konsentrasi dari atas
+        hasil,
         bobot_molekul_titrat_kadar,
         faktor_pengali_kadar,
         volume_titrat_kadar
