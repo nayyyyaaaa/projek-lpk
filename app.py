a338e682ddc01368eb836a902326bbb96f74f878
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-24 18:38:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ s.markdown("""
 s.markdown('<p class="centered-title">Aplikasi Perhitungan Konsentrasi Normalitas, %Kadar, dan %RPD pada Titrasi ∞</p>', unsafe_allow_html=True)
 
 s.markdown('<p class="centered-subtitle">Perhitungan Konsentrasi Normalitas</p>', unsafe_allow_html=True)
-s.title("Aplikasi Perhitungan Konsentrasi Normalitas, %Kadar, dan %RPD pada Titrasi")
 
 # ----------------------------
 # Fungsi Perhitungan
