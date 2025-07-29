51acc94833bee10f78979828e743b1adb680cb2c
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-29 12:56:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def get_base64(file_path):
     return base64.b64encode(data).decode()
 
 # ubah sesuai nama file gambarmu
-bg_image = get_base64(r"C:\\projectlpk\\backgroundkimia.jpg")
+bg_image = get_base64(r"C:\projectlpk\backgroundkimia.jpg")
 
 # CSS & Gaya Imut Kimia
 s.markdown(f"""
