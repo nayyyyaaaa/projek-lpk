8f954aecc3f65fbbb962883effe94b2fbcf679c1
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-29 14:17:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ s.markdown(f"""
 
 .block-container {{
     background-image: url("data:image/jpg;base64,{bg_image}");
-    background-size: contain;
+    background-size: 80%;
     background-repeat: no-repeat;
     background-attachment: scroll;
     background-position: center bottom;
