2ecefec992a3b143f7de7545de3a945b0509fd74
nayyyyaaaa <nailah.inaaya@gmail.com>
2025-07-30 14:03:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,13 +115,8 @@ s.markdown("###### pada web aplikasi ini rumus yang diaplikasikan adalah:")
 s.markdown("1. Normalitas (N):")
 s.latex(r"N = \frac{\text{bobot titrat(mg)}}{\text{faktor pengencer/pengali(FP)} \times \text{volume titran(mL)}\times \text{bobot ekivalen(BE)}}")
 
-s.markdown(r'''
-<div style="font-size: 14px;">
-\[
-\% \text{kadar} = \frac{\textit{konsentrasi }(N/M) \times \text{volume titran (mL)} \times \text{bobot ekivalen (BE)} \times 0.1}{\text{volume titrat (mL)}}
-\]
-</div>
-''', unsafe_allow_html=True)
+s.markdown("2. Kadar (%):")
+s.latex(r"\% \text{kadar} = \frac{konsetrasi(N/M) \times \text{volume titran(mL)} \times \text{bobot ekivalen(BE)} \times 0.1 \times \text{faktor pengencer/pengali(FP)}} {\text{volume titrat(mL)}}")
 
 s.markdown("3. %RPD (Relative Percent Difference):")  
 s.latex(r"\% \text{RPD} = \frac{| \text{konsentrasi awal(N1)} - \text{konsentrasi duplo(N2)}|} {\left(\frac{\text{N}_1 + \text{N}_2} {2}\right)} \times 100")
